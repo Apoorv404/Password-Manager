28643e980775f720861cc000724741ea5220d8e8
Apoorv404 <jadhavapoorv2244@gmail.com>
2025-04-20 09:44:53 +0530

    added search functionality

--- main.py
+++ main.py
@@ -63,6 +63,23 @@ def save():
                     website_entry.delete(0, END)
                     password_entry.delete(0, END)
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -99,6 +116,9 @@ password_entry.grid(row=3, column=1, columnspan=1)
 #Buttons
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=3, column=2)
+
+search_button = Button(text="Search", command=find_password, width=13)
+search_button.grid(row=1, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
